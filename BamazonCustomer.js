d6ed363eb258c244d0e7a3898fd7e8a60e7aab36
Aaron Lee <worldedit05@gmail.com>
2016-10-25 00:37:59 -0500

    Added prompts for the checkout process

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -11,9 +11,47 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-
+    console.log("Sucessful connection");
+    showProducts();
+    setTimeout(runCheckOut, 1000);
 });
 
-var checkOut = function() {
+var runCheckOut = function() {
+    inquirer.prompt([{
+        name: "item",
+        type: "input",
+        message: "What itemID would you like to purchase?"
+    }, {
+        name: "quantity",
+        type: "type",
+        message: "How many?"
+    }]).then(function(answer) {
+        var updateStock;
+        connection.query("SELECT * FROM products WHERE ?", {
+            ItemID: answer.item
+        }, function(err, res) {
+            updateStock = res[0].StockQuantity - parseInt(answer.quantity);
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                StockQuantity: parseInt(updateStock)
+            }, {
+                ItemID: answer.item
+            }], function(err, res) {
+                console.log(res + "\nStock has been deducted to " + updateStock);
+                showProducts();
+                setTimeout(runCheckOut, 1000);
+            });
+        });
 
+    });
+
+};
+
+var showProducts = function() {
+    console.log('\nHere is the catalog: \n\nProductID || Product Name || Deparment || Price || Stock\n');
+    var query = 'SELECT * FROM products';
+    connection.query(query, function(err, res) {
+        res.forEach(function(item, index) {
+            console.log(item.ItemID + ' || ' + item.ProductName + ' || ' + item.DepartmentName + ' || ' + item.Price + ' || ' + item.StockQuantity);
+        });
+    });
 };
